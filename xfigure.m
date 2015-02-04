522c09b120fd24207a6eb1841ed76b5edf12f6a1
Mirza <mirza.cenanovic@gmail.com>
2015-02-04 11:25:48 +0100

    xfigure v1.3

---
+++ xfigure.m
@@ -0,0 +1,402 @@
+function [h, this] = xfigure(varargin)
+% xfigure
+% xfigure(fignumber)
+% xfigure(id) where id is a string e.g. xfigure('Mesh')
+% h = xfigure(...)
+% [h, xfigure_This] = xfigure(...)
+%
+%
+% Press "G" to toggle grid
+% Press "CTRL+R" to reset axis
+% Press "Middlemousebuton and drag" to pan axis
+% Press "CTRL+Click and drag" to rotate axis
+% Press "SHIFT+S" snap the view to the view-box
+% Press "SHIFT+P" to save the current window position
+%
+% Version 1.3, created by Mirza Cenanovic
+
+%% Initiate
+global xfigure_This
+
+xfigure_This.blankWindow = 0;
+figureIDIsProvided = 0;
+if nargin >= 1
+    param1 = varargin{1};
+    if isa(param1,'char')
+        fighand = strToHandleID(param1);
+        figureIDIsProvided = 1;
+        if ishandle(fighand)
+            figure(fighand)
+            h = fighand; this = h;
+            return
+        end
+    elseif isa(param1,'numeric')
+        fighand = param1;
+        if ishandle(fighand)
+            figure(fighand)
+            h = fighand; this = h;
+            return
+        end
+    end
+    
+    xfigure_This.gui = figure(fighand);
+    h = xfigure_This.gui;
+    
+    if nargin > 1
+        varargin = varargin(2:end);
+    end
+else
+    xfigure_This.gui = figure;
+    xfigure_This.blankWindow = 1;
+    h = xfigure_This.gui;
+end
+
+
+%% Figure properties
+xfigure_This.bcolor = get(xfigure_This.gui,'Color');
+if figureIDIsProvided > 0
+    if verLessThan('matlab','8.4')
+        set(xfigure_This.gui,'Tag',param1)
+    else
+        xfigure_This.gui.Tag = param1;
+    end
+else
+    if verLessThan('matlab','8.4')
+        set(xfigure_This.gui,'Tag',num2str(h))
+    else
+        xfigure_This.gui.Tag = num2str(h.Number);
+    end
+end
+set(xfigure_This.gui,'ToolBar','figure')
+if isenabled('Title', varargin)
+    set(xfigure_This.gui, 'Name', getoption('Title', varargin))
+elseif figureIDIsProvided > 0
+    set(xfigure_This.gui, 'Name', param1)
+end
+
+
+
+%% Figure position
+xfigure_This.filename = ['XFig',get(xfigure_This.gui,'Tag'),'Data.mat'];
+if exist(xfigure_This.filename,'file') == 2
+    S = load(xfigure_This.filename);
+    WP = S.WindowPosition;
+    set(xfigure_This.gui, 'Position', WP)
+end
+xfigure_This.WindowPosition = get(xfigure_This.gui, 'Position');
+
+
+%% Axes
+xfigure_This.axes = axes;
+xfigure_This.axis = axis;
+
+
+
+%% Help text
+xfigure_This.helpText = {'Press "G" to toggle grid'...
+    'Press "CTRL+R" to reset axis'...
+    'Press "Middlemousebuton and drag" to pan axis'...
+    'Press "CTRL+Click and drag" to rotate axis'...
+    'Press "SHIFT+S" snap the view to the view-box'...
+    'Press "SHIFT+P" to save the current window position'};
+xfigure_This.uiTextHelp = uicontrol('style','text','string',xfigure_This.helpText,'HorizontalAlignment','Left',...
+    'Position', [5,5,500,64],'Visible','off');
+ext1 = get(xfigure_This.uiTextHelp,'Extent');
+set(xfigure_This.uiTextHelp,'Position',[5,20,ext1(3),ext1(4)])
+
+%% Status text
+xfigure_This.statusText = ' ';
+xfigure_This.uistatusText = uicontrol('style','text','string',xfigure_This.statusText,'HorizontalAlignment','Left',...
+    'Position', [7,5,20,20],'Visible','on');
+set(xfigure_This.uistatusText,'BackgroundColor',xfigure_This.bcolor)
+xfigure_This.statusTextWidth = xfigure_This.WindowPosition(3)-2*5;
+xfigure_This.statusTextHeight = 15;
+set(xfigure_This.uistatusText,'Position',[5, 5, xfigure_This.statusTextWidth, xfigure_This.statusTextHeight])
+
+
+%% Axis properties
+set(xfigure_This.axes, 'Units', 'pixels');
+set(xfigure_This.axes, 'TickDir','out');
+% set(xfigure_This.axes,'XColor', xfigure_This.bcolor,'YColor', xfigure_This.bcolor,'ZColor', xfigure_This.bcolor)
+
+xfigure_This.axesMarginSides = 60; %pixels
+xfigure_This.axesMarginTop = 40;
+xfigure_This.axesMarginBottom = xfigure_This.statusTextHeight + xfigure_This.axesMarginSides;
+
+xfigure_This.axesWidth = xfigure_This.WindowPosition(3)-xfigure_This.axesMarginSides*2;
+xfigure_This.axesHeight = xfigure_This.WindowPosition(4) - xfigure_This.axesMarginBottom - xfigure_This.axesMarginTop;
+
+set(xfigure_This.axes,'Position', [xfigure_This.axesMarginSides, xfigure_This.axesMarginBottom, xfigure_This.axesWidth,xfigure_This.axesHeight])
+
+
+% xlabel('X')
+% ylabel('Y')
+% zlabel('Z')
+% title('Hello')
+
+%
+%% Properties
+xfigure_This.rotate3d = 0;
+xfigure_This.hrot = 0;
+xfigure_This.grid = 0;
+xfigure_This.CameraViewAngleDefault = get(xfigure_This.axes,'CameraViewAngle');
+xfigure_This.CameraViewAngle = xfigure_This.CameraViewAngleDefault;
+
+%% Original figure state
+xfigure_This.axO = xfigure_This.axis;
+[xfigure_This.azO,xfigure_This.elO] = view;
+xfigure_This.axesPosO = get(xfigure_This.axes,'Position');
+
+try
+    xfigure_This.PanStart = get(gca,'Position');
+    xfigure_This.PanStartX = xfigure_This.PanStart(1);
+    xfigure_This.PanStartY = xfigure_This.PanStart(2);
+catch
+    xfigure_This.PanStartX = 0;
+    xfigure_This.PanStartY = 0;
+end
+
+%% Listeners
+set(xfigure_This.gui, 'KeyPressFcn',@KeyPressFcn)
+set(xfigure_This.gui, 'WindowScrollWheelFcn',@ScrollFcn)
+set(xfigure_This.gui, 'WindowButtonDownFcn',@buttonDownFcn)
+set(xfigure_This.gui, 'WindowButtonUpFcn',@buttonUpFcn)
+set(xfigure_This.gui, 'ResizeFcn', @ResizeFcn)
+% set(xfigure_This.gui, 'CloseRequestFcn',@mainCloseReq)
+this = xfigure_This;
+    
+%% CallBackFunctions
+    function KeyPressFcn(source,event)
+        xfigure_KPF(source, event);
+        
+    end
+
+
+
+%% CallBackFunctions
+
+    function buttonDownFcn(varargin)
+        modifier = get(xfigure_This.gui,'currentModifier');
+        if isempty(modifier)
+            modifier = 0;
+        end
+        selection = get(xfigure_This.gui,'SelectionType');
+        
+        %% Middlemouse
+        if strcmpi(selection,'extend')
+            
+            xfigure_This.panSensitivity = 1;
+            
+            set(xfigure_This.gui,'Pointer','fleur')
+            set(xfigure_This.gui,'WindowButtonMotionFcn', @PanWindowButtonFcn)
+            
+            
+            
+            crd = get(xfigure_This.gui, 'CurrentPoint');
+            x = crd(1);
+            y = crd(2);
+            
+            xfigure_This.xstartPos = x;
+            xfigure_This.ystartPos = y;
+            
+%             
+%             % Disable rotation
+%             try set(xfigure_This.hrot,'Enable','off'); end
+%             xfigure_This.rotate3d = 0;
+        end
+        
+        %% CTRL + Middlemouse = rotate
+        if (strcmpi(selection,'alt') || strcmpi(selection,'open')) && strcmp(modifier,'control')
+            xfigure_This.rotateSensitivity = 0.5;
+            
+            set(xfigure_This.gui,'Pointer','circle')
+            set(xfigure_This.gui,'WindowButtonMotionFcn', @RotateWindowButtonFcn)
+            
+            crd = get(xfigure_This.gui, 'CurrentPoint');
+            
+            x = crd(1);
+            y = crd(2);
+            xfigure_This.xs = x;
+            xfigure_This.ys = y;
+            [xfigure_This.az,xfigure_This.el] = view;
+            try
+                set(xfigure_This.uistatusText, 'String', ['Az: ',num2str(xfigure_This.az), ' El: ', num2str(xfigure_This.el) ])
+            catch
+%                 disp(['Az: ',num2str(xfigure_This.az), ' El: ', num2str(xfigure_This.el) ])
+            end
+        end      
+    end
+
+    function buttonUpFcn(varargin)
+        set(xfigure_This.gui,'WindowButtonMotionFcn','');
+        set(xfigure_This.gui,'Pointer','arrow')
+        try
+            set(xfigure_This.uistatusText, 'String', [' '])
+        end
+        
+    end
+    
+    function PanWindowButtonFcn(varargin)
+        
+        try
+        crd = get(xfigure_This.gui, 'CurrentPoint');
+        x = crd(1);
+        y = crd(2);
+        dx = -(xfigure_This.xstartPos - x);
+        dy = -(xfigure_This.ystartPos - y);
+        xfigure_This.xstartPos = x;
+        xfigure_This.ystartPos = y;
+        
+        
+        Pos = get(gca,'Position');
+        
+        Pos = Pos + xfigure_This.panSensitivity * [dx,dy,0,0];
+        
+        set(gca,'Position', Pos)
+        end
+        
+    end
+
+    function RotateWindowButtonFcn(varargin)
+        
+        crd = get(xfigure_This.gui, 'CurrentPoint');
+        x = crd(1);
+        y = crd(2);
+        dx = (xfigure_This.xs - x);
+        dy = (xfigure_This.ys - y);
+        xfigure_This.xs = x;
+        xfigure_This.ys = y;
+        
+%         daz = sign(dx);
+%         del = sign(dy);
+
+        daz = dx;
+        del = dy;
+
+        %         [daz,del]*xfigure_This.rotateSensitivity;
+        xfigure_This.az = xfigure_This.az + round(daz * xfigure_This.rotateSensitivity);
+        xfigure_This.el = xfigure_This.el + round(del * xfigure_This.rotateSensitivity);
+        
+        view(xfigure_This.az,xfigure_This.el)
+        
+        if xfigure_This.az > 360
+            xfigure_This.az = xfigure_This.az - 360;
+        end
+        if xfigure_This.az < -360
+            xfigure_This.az = xfigure_This.az + 360;
+        end
+        if xfigure_This.el > 360
+            xfigure_This.el = xfigure_This.el - 360;
+        end
+        if xfigure_This.el < -360
+            xfigure_This.el = xfigure_This.el + 360;
+        end
+        try
+            set(xfigure_This.uistatusText, 'String', ['Az: ',num2str(xfigure_This.az), ' El: ', num2str(xfigure_This.el) ])
+        catch
+%             disp(['Az: ',num2str(xfigure_This.az), ' El: ', num2str(xfigure_This.el) ])
+        end
+    end
+
+    function ScrollFcn(varargin)
+        %% Zoom factor:
+        zf = .5;
+        
+        controlIsPressed = 0;
+        modifier = get(xfigure_This.gui,'currentModifier');
+        if ~isempty(modifier)
+            switch modifier{1}
+                case 'control'
+                    controlIsPressed = 1;
+            end
+        end
+        
+        if varargin{2}.VerticalScrollCount < 0 && controlIsPressed
+            % zoom in
+            if xfigure_This.CameraViewAngle > 1
+                xfigure_This.CameraViewAngle = xfigure_This.CameraViewAngle - zf;
+            elseif xfigure_This.CameraViewAngle < 1
+                xfigure_This.CameraViewAngle = xfigure_This.CameraViewAngle - zf/10;
+            end
+            
+            if xfigure_This.CameraViewAngle < 0
+                xfigure_This.CameraViewAngle = 0.001;
+            end
+            try
+                set(xfigure_This.axes,'CameraViewAngle',xfigure_This.CameraViewAngle)
+            catch
+                zoom(1.1)
+            end
+            
+        elseif varargin{2}.VerticalScrollCount > 0 && controlIsPressed
+            % zoom out
+            if xfigure_This.CameraViewAngle < 1
+                xfigure_This.CameraViewAngle = xfigure_This.CameraViewAngle + zf/10;
+            elseif xfigure_This.CameraViewAngle >= 1
+                xfigure_This.CameraViewAngle = xfigure_This.CameraViewAngle + zf;
+            end
+            try
+                set(xfigure_This.axes,'CameraViewAngle',xfigure_This.CameraViewAngle)
+            catch
+                zoom(0.8)
+            end
+            
+        end
+    end
+
+    function ResizeFcn(varargin)
+        xfigure_This.WindowPosition = get(xfigure_This.gui,'Position');
+        xfigure_This.statusTextWidth = xfigure_This.WindowPosition(3)-2*5;
+        try
+            set(xfigure_This.uistatusText,'Position',[5, 5, xfigure_This.statusTextWidth, xfigure_This.statusTextHeight])
+        end
+        xfigure_This.axesWidth = xfigure_This.WindowPosition(3)-xfigure_This.axesMarginSides*2;
+        xfigure_This.axesHeight = xfigure_This.WindowPosition(4) - xfigure_This.axesMarginBottom - xfigure_This.axesMarginTop;
+        try
+            set(xfigure_This.axes,'Position', [xfigure_This.axesMarginSides, xfigure_This.axesMarginBottom, xfigure_This.axesWidth,xfigure_This.axesHeight])
+        end
+    end
+
+
+end
+
+
+function rv = isenabled(mode, varargin)
+if nargin < 1
+    error('No arguments')
+end
+varargin = varargin{:};
+
+ind = find(strcmpi(varargin,mode), 1);
+if ~isempty(ind)
+    rv = 1;
+else
+    rv = 0;
+end
+end
+function param = getoption(mode, varargin)
+varargin = varargin{:};
+ind1 = find(strcmpi(varargin,mode), 1);
+if ~isempty(ind1)
+    %Errorhandling
+    if ~iscell(varargin)
+        varargin = {varargin};
+    end
+    if ind1+1 <= length(varargin)
+        param = varargin{ind1+1};
+    else
+        %         error(['No options are followed by the property ''', mode,''' '])
+        param = [];
+    end
+else
+    param = [];
+end
+end
+
+function h = strToHandleID(str)
+h = round(norm(double(str)-44,1));
+if h > intmax-1
+    error('h is exceeding intmax!')
+end
+end
+
---
+++ xfigure_KPF.m
@@ -0,0 +1,116 @@
+function xfigure_KPF(source,event)
+% xfigure_KPF KeyPressCallback_3D
+% Used by xfigure
+
+global xfigure_This
+%% modifier
+modifier = get(source,'currentModifier');
+if isempty(modifier)
+    modifier = 0;
+end
+
+%% CTRL + R reset axis
+if strcmp(modifier,'control') && strcmp(event.Key,'r')
+    try
+        ch = get(xfigure_This.axes, 'Children');
+        if numel(ch) > 0
+            xmax = zeros(numel(ch),1);
+            ymax = xmax; zmax = ymax; xmin = xmax; ymin = xmax; zmin = zmax;
+            
+            for i = 1:numel(ch)
+                try xd = get(ch(i),'XData'); end
+                try yd = get(ch(i),'YData'); end
+                try zd = get(ch(i),'ZData'); end
+                try
+                    xd = xd(:);
+                    xmax(i) = max(xd);
+                    xmin(i) = min(xd);
+                end
+                try
+                    yd = yd(:);
+                    ymax(i) = max(yd);
+                    ymin(i) = min(yd);
+                end
+                try
+                    zd = zd(:);
+                    zmax(i) = max(zd);
+                    zmin(i) = min(zd);
+                end
+                
+            end
+            xfigure_This.axis = [xmin, xmax, ymin, ymax, zmin, zmax];
+            axis(xfigure_This.axis)
+        end
+    catch
+        zoom reset
+        
+        try
+            set(gca,'Position',xfigure_This.PanStart)
+        end
+        
+    end
+    
+    view([xfigure_This.azO,xfigure_This.elO] );
+    set(xfigure_This.axes,'Position',xfigure_This.axesPosO);
+    
+end
+
+%% SHIFT + P
+if strcmp(modifier,'shift') && strcmp(event.Key,'p')
+    WindowPosition = get(xfigure_This.gui,'Position');
+    save(xfigure_This.filename, 'WindowPosition')
+    try
+        set(xfigure_This.uistatusText, 'String', ['Figure position saved to: ', xfigure_This.filename])
+    catch
+        disp(['Figure position saved to: ', xfigure_This.filename])
+    end
+end
+
+%% h - help
+if strcmp(event.Key,'h')
+    if strcmpi(get(xfigure_This.uiTextHelp,'Visible'),'off')
+        set(xfigure_This.uiTextHelp,'Visible','on')
+    else
+        set(xfigure_This.uiTextHelp,'Visible','off')
+    end
+end
+
+%% g - grid & axis
+if strcmp(event.Key,'g')
+    gridf = getappdata(xfigure_This.gui, 'grid');
+    if ~isempty(gridf)
+        xfigure_This.grid = gridf;
+    end
+    
+    switch mod(xfigure_This.grid,4)
+        case 0
+            grid(xfigure_This.axes,'On');
+            set(gca,'Visible','on')
+        case 1
+            grid(xfigure_This.axes,'minor');
+        case 2
+            grid(xfigure_This.axes,'Off');
+            %                     set(xfigure_This.axes,'XColor', xfigure_This.bcolor,'YColor', xfigure_This.bcolor, 'ZColor', xfigure_This.bcolor)
+        case 3
+            grid(xfigure_This.axes,'Off');
+            %                     set(xfigure_This.axes,'XColor', 'black','YColor', 'black', 'ZColor', 'black')
+            set(gca,'Visible','off')
+    end
+    xfigure_This.grid = xfigure_This.grid +1;
+    setappdata(xfigure_This.gui, 'grid', xfigure_This.grid)
+end
+
+%% SHIFT + s - normal to
+if strcmp(modifier,'shift') && strcmp(event.Key,'s')
+    [xfigure_This.az,xfigure_This.el] = view;
+    v = [xfigure_This.az,xfigure_This.el];
+    roundTargets = [-360 -270 -180 -90 0 90 180 270 360];
+    vRounded = interp1(roundTargets,roundTargets,v,'nearest');
+    xfigure_This.az = vRounded(1);
+    xfigure_This.el = vRounded(2);
+    view([xfigure_This.az, xfigure_This.el]);
+    try
+        set(xfigure_This.uistatusText, 'String', ['Az: ',num2str(xfigure_This.az), ' El: ', num2str(xfigure_This.el) ])
+    end
+end
+
